62f221f10756f0a2ffbafe90c41e0fd147f53064
Shane Tomlinson <stomlinson@mozilla.com>
2015-05-05 20:26:13 +0100

    feat(client): Sync over WebChannel glue
    Add a new broker, the FxDesktopV2AuthenticationBroker that is used whenever
    communicating with the browser using WebChannels and the new commands.
    
    issue #2231

--- app/tests/test_start.js
+++ app/tests/test_start.js
@@ -101,6 +101,7 @@ function (Translator, Session) {
     '../tests/spec/models/reliers/fx-desktop',
     '../tests/spec/models/auth_brokers/base',
     '../tests/spec/models/auth_brokers/fx-desktop',
+    '../tests/spec/models/auth_brokers/fx-desktop-v2',
     '../tests/spec/models/auth_brokers/web-channel',
     '../tests/spec/models/auth_brokers/redirect',
     '../tests/spec/models/auth_brokers/oauth',
